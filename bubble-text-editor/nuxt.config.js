83e188b601d371f3a2c9941a8a4e3324425c1e13
Giang Huynh <trgianghuynhdev1808@gmail.com>
2021-03-19 11:44:33 +0700

    imp: init vue-quill-editor

---
+++ bubble-text-editor/nuxt.config.js
@@ -0,0 +1,33 @@
+export default {
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: "bubble-text-editor",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: ["quill/dist/quill.bubble.css"],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [{ src: "~plugins/nuxt-quill-plugin", ssr: false }],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {}
+};
---
+++ bubble-text-editor/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <Editor />
+  </div>
+</template>
+
+<script>
+import Editor from "~/components/Editor";
+
+export default {
+  components: {
+    Editor
+  }
+};
+</script>
+
+<style></style>
